0416a65a982df4ec35890749c749fb3fa6b7a563
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 04:35:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,4 +10,8 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  number = 1
+  array.each do |element|
+    puts "#{number}. #{element}"
+    number +=1
 end
